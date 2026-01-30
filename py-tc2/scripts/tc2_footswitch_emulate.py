2e018cc9423857b0ad1ce7827170b9ad38f24ca9
ca3rine <ca3rine@gmail.com>
2026-01-30 22:29:43 +0100

    fixed the active-weft problem, tested and is working!

---
+++ py-tc2/scripts/tc2_footswitch_emulate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# (c) 2024 Alex McLean and Pei-Ying Lin
+
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# <http://www.gnu.org/licenses/>.
+
+import socket, re, random, sys
+import paho.mqtt.client as mqtt
+import argparse, json
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--host")
+parser.add_argument("--port")
+parser.add_argument("--username")
+parser.add_argument("--password")
+args=parser.parse_args()
+
+mqtt_host = args.host or "slab.org"
+mqtt_username = args.username or "tue"
+mqtt_password = args.password
+mqtt_port = 1883
+
+subscribe_topics = ["/pattern", "/pattern/json", "/tc2/request-status", "/tc2/stop", "/tc2/footswitch"]
+
+## mqtt connect
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    for topic in subscribe_topics:
+        client.subscribe(topic)
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.username_pw_set(username=mqtt_username, password=mqtt_password)
+mqttc.connect(mqtt_host, mqtt_port, 60)
+mqttc.publish("/tc2/footswitch","0")
+
+print("Waiting for data..")
+run = True
+while run:
+    rc = mqttc.loop(timeout=0)
+    if rc != 0:
+        print("mqtt error")
+        run = False
+    # blocks for up to 1/20th of a second
